ea3469b3e549321e4a9767b12eb799e1750745f5
Lekhya25 <lekhyam11l@gmail.com>
2024-02-13 20:09:59 +0530

    Added code for menubar

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "gsol");
+    gtk_header_bar_set_title(header_bar, "resurge2");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "gsol");
+    gtk_window_set_title(window, "resurge2");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
